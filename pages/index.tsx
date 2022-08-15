4ad3d98ac3a4f37c1fb594d908994ff0a7ec4cd9
Michael <trinh.michael98@gmail.com>
2022-08-15 01:34:49 -0700

    removed lottie loading animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, Suspense } from "react";
 import PageHead from "@/components/_head";
 import { useLocomotiveScroll } from "react-locomotive-scroll";
 
@@ -8,11 +8,8 @@ import Skills from "@/components/homeSkills";
 import WorksSummary from "@/components/worksSummary";
 import WorksShowcase from "@/components/worksShowcase";
 import Contact from "@/components/contact";
-import LottieLoading from "@/components/lottieLoading/index.server.js";
 
 export default function Test(): JSX.Element {
-  const [loadedWindow, setLoadedWindow] = useState(false);
-
   const { scroll } = useLocomotiveScroll();
   const homeSectionRef = useRef<HTMLDivElement>(null);
   const skillsSectionRef = useRef<HTMLElement>(null);
@@ -23,49 +20,27 @@ export default function Test(): JSX.Element {
     if (scroll) scroll.update();
   }, [scroll]);
 
-  useEffect(() => {
-    const onPageLoad = () => {
-      if (loadedWindow === false) setLoadedWindow(true);
-      else null;
-    };
-
-    if (document.readyState === "complete") {
-      setTimeout(() => {
-        onPageLoad();
-      }, 1500);
-    } else {
-      window.addEventListener("load", onPageLoad);
-      return () => window.removeEventListener("load", onPageLoad);
-    }
-  }, []);
-
   const handleNavHomeClick = () => scroll.scrollTo(homeSectionRef.current, 0);
   const handleNavSkillsClick = () => scroll.scrollTo(skillsSectionRef.current, 0);
   const handleNavWorksClick = () => scroll.scrollTo(worksSectionRef.current, 0);
   const handleNavContactClick = () => scroll.scrollTo(contactSectionRef.current, 0);
 
   return (
-    <>
-      {loadedWindow === false ? (
-        <LottieLoading />
-      ) : (
-        <React.Fragment>
-          <PageHead title="Michael Trinh's Portfolio" />
+    <React.Fragment>
+      <PageHead title="Michael Trinh's Portfolio" />
 
-          <NavBar
-            navHome={handleNavHomeClick}
-            navSkills={handleNavSkillsClick}
-            navWorks={handleNavWorksClick}
-            navContact={handleNavContactClick}
-          />
+      <NavBar
+        navHome={handleNavHomeClick}
+        navSkills={handleNavSkillsClick}
+        navWorks={handleNavWorksClick}
+        navContact={handleNavContactClick}
+      />
 
-          <Header ref={homeSectionRef} />
-          <Skills ref={skillsSectionRef} />
-          <WorksSummary ref={worksSectionRef} />
-          <WorksShowcase />
-          <Contact ref={contactSectionRef} />
-        </React.Fragment>
-      )}
-    </>
+      <Header ref={homeSectionRef} />
+      <Skills ref={skillsSectionRef} />
+      <WorksSummary ref={worksSectionRef} />
+      <WorksShowcase />
+      <Contact ref={contactSectionRef} />
+    </React.Fragment>
   );
 }
